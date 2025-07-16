18640187b16eec7042617fe489fbc25866cfc1a3
Dhruv Kumar <dhuruv.kumar@dhuruvvrs-31.successivetech.com>
2025-07-16 10:32:41 +0530

    refactor : fixed routes pattern by added /user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,18 +2,18 @@ import express from "express";
 
 import { authMiddleware } from "../middlewares/auth";
 import { login, register, user } from "../controllers/user";
-import { dataSeed } from "../controllers/dataSeed";
+import { info } from "../controllers/dataSeed";
 import { loggerMiddleware } from "../middlewares/logger";
 
 const router = express.Router();
 
-router.post("/login", login);
+router.post("/user/login", login);
 router.get("/user", authMiddleware, user);
-router.post("/data-seed", loggerMiddleware, dataSeed);
-router.post("/register", register);
+router.post("/user/info", loggerMiddleware, info);
+router.post("/user/register", register);
 
 // middleware chaining
 
-router.post("/dashboard", loggerMiddleware, authMiddleware, user);
+router.post("/user/dashboard", loggerMiddleware, authMiddleware, user);
 
 export { router };
